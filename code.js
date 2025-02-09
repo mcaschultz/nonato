26e564463424a6c8ee3fd214ec2b37350653034e
Marcelo Jorge Carneiro Schultz de Almeida <mcaschultz@hotmail.com>
2025-02-09 09:32:52 -0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,6 @@
+console.log("oi");
+function clicouEstrela() {
+    console.log("clicou");
+    document.getElementById("parteUm").style.display = "none"
+    document.getElementById("parteDois").style.display = "block"
+}
